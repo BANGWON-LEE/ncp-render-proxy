cbc8180c6bfce1b109db3487e0677ee6ee4977de
BANGWON LEE <qkddnjs31@gmail.com>
2025-06-05 13:27:45 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const axios = require('axios')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/driving', async (req, res) => {
+  const { start, goal } = req.query
+
+  try {
+    const result = await axios.get(
+      `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${start}&goal=${goal}`,
+      {
+        headers: {
+          'X-NCP-APIGW-API-KEY-ID': process.env.NCP_APIGW_API_KEY_ID,
+          'X-NCP-APIGW-API-KEY': process.env.NCP_APIGW_API_KEY,
+        },
+      }
+    )
+
+    res.json(result.data)
+  } catch (err) {
+    res.status(500).json({ message: '프록시 서버 오류', detail: err.message })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Proxy server running on ${PORT}`)
+})
