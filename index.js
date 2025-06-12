20fc65c3556623088d991d885c92299929a24e41
BANGWON LEE <qkddnjs31@gmail.com>
2025-06-12 20:22:23 +0900

    걷기 추가

--- index.js
+++ index.js
@@ -20,13 +20,51 @@ function reducePrecision(path) {
     parseFloat(lat.toFixed(5)),
   ])
 }
-app.disable('x-powered-by')
 app.get('/driving', async (req, res) => {
   const { start, goal } = req.query
 
   try {
     const result = await axios.get(
       `https://maps.apigw.ntruss.com/map-direction/v1/driving?goal=${goal}&start=${start}`,
+      {
+        headers: {
+          'x-ncp-apigw-api-key-id': process.env.NCP_APIGW_API_KEY_ID,
+          'x-ncp-apigw-api-key': process.env.NCP_APIGW_API_KEY,
+        },
+      }
+    )
+
+    const route = result.data.route?.traoptimal?.[0]
+    const fullPath = route.path || []
+    const simplified = downsamplePath(fullPath, 200)
+
+    const summary = {
+      distance: route.summary?.distance,
+      duration: route.summary?.duration,
+      start: route.summary?.start,
+      goal: route.summary?.goal,
+    }
+    const reduced = reducePrecision(simplified)
+
+    res.json({
+      summary,
+      path: reduced,
+    })
+  } catch (err) {
+    console.error('[DRIVING ERROR]', err.response?.data || err.message)
+    res.status(500).json({
+      message: '프록시 서버 오류',
+      detail: err.response?.data || err.message,
+    })
+  }
+})
+
+app.get('/walking', async (req, res) => {
+  const { start, goal } = req.query
+
+  try {
+    const result = await axios.get(
+      `https://maps.apigw.ntruss.com/map-direction/v1/walking?goal=${goal}&start=${start}`,
       {
         headers: {
           'x-ncp-apigw-api-key-id': process.env.NCP_APIGW_API_KEY_ID,
